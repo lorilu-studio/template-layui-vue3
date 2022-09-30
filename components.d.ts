38220dc6c7f5c8110480e4eb9c80f0354e28aeb5
就眠儀式 <854085467@qq.com>
2022-10-01 02:05:18 +0800

    升级 layui-vue 1.5.1

--- components.d.ts
+++ components.d.ts
@@ -3,6 +3,8 @@
 // Read more: https://github.com/vuejs/core/pull/3399
 import '@vue/runtime-core'
 
+export {}
+
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
     LayBadge: typeof import('@layui/layui-vue')['LayBadge']
@@ -57,5 +59,3 @@ declare module '@vue/runtime-core' {
     RouterView: typeof import('vue-router')['RouterView']
   }
 }
-
-export {}
