734bfd490bcb99d094b751ae754c4af1824a9b91
就眠儀式 <854085467@qq.com>
2023-02-08 10:11:11 +0800

    update

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,12 @@ import Router from './router'
 import Store from './store'
 import App from './App.vue'
 import { permission } from "./directives/permission";
-import Layui from "@layui/layui-vue";
-import '@layui/layui-vue/lib/index.css'
 import './mockjs'
 
 const app = createApp(App)
 
-app.use(Layui);
-app.use(Router);
 app.use(Store);
+app.use(Router);
 
 app.directive("permission",permission);
 app.mount('#app');
